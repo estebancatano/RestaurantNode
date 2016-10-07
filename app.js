3befabe2d7fcb79fdbabf1d2b8421cd56a7c936f
Esteban Cataño Escobar <esteban.catanoe@gmail.com>
2016-10-07 17:48:39 -0500

    Cambio en el puerto

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var routes = require('./routes');
 
 var app = express();
 
-app.listen(8080, function() {
-	console.log('Puerto 8080 escuchando');
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+	console.log('Puerto ' + app.get('port') + ' escuchando');
 });
 
 app.use(bodyParser.urlencoded({extended: true}));
