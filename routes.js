257379be88cfaaf6b953c691affb8ba16cde463c
Esteban Cataño Escobar <esteban.catanoe@gmail.com>
2016-10-07 18:22:44 -0500

    Corrección routes.js

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-var dbRestaurant = require('./queriesRestaurant');WWW
+var dbRestaurant = require('./queriesRestaurant');
 router.get('/api/restaurants', dbRestaurant.getAllRestaurants);
 router.get('/api/restaurants/:name', dbRestaurant.getRestaurantByName);
 router.post('/api/restaurants', dbRestaurant.createRestaurant);
