ea6a25096a58312f206a9bf89648b5527287b25f
joan56 <armj.36@gmail.com>
2016-10-28 17:52:30 -0500

    Solución al CORS

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var routesRestaurant = require('./routes/routesRestaurant');
 var routesTable = require('./routes/routesTable');
+var cors = require('cors');
 
 var app = express();
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,6 +15,7 @@ app.listen(app.get('port'), function() {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api/restaurants/', routesRestaurant);
-app.use('/api/tables/',routesTable)
+app.use('/api/tables/',routesTable);
+app.use(cors());
 
 module.exports = app;
